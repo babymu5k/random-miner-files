5b5cdcdc68d445537e04168c3f0151722dd3e65e
superpythonguy <95881676+superpythonguy@users.noreply.github.com>
2021-12-12 09:58:34 +0000

    Update javascript_test.js

--- javascript_test.js
+++ javascript_test.js
@@ -1,3 +1,4 @@
-
-// the hello world program
 console.log('Duinocoin is the best');
+console.log("this is all i know about JS")
+const a = Math.floor(Math.random() * (10 - 1)) + 1;
+console.log(`Random value between 1 and 10 is ${a}`);

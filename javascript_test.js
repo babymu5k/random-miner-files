a6fe670b9022cde78432ac472829c5a426ca14cf
superpythonguy <95881676+superpythonguy@users.noreply.github.com>
2022-02-03 16:50:11 +0000

    Delete javascript_test.js

--- javascript_test.js
+++
@@ -1,5 +0,0 @@
-console.log('Duinocoin is the best');
-console.log("this is all i know about JS")
-const a = Math.floor(Math.random() * (10 - 1)) + 1;
-console.log(`Random value between 1 and 10 is {a}`);
-console.log("use my miner pls")
